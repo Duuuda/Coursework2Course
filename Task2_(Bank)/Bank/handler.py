428898fcf9f242876be3c7add183e244c9357ab0
Duuuda <supermen.tut.103103@gmail.com>
2021-05-12 17:22:55 +0300

    Remove old Handler

--- Task2_(Bank)/Bank/handler.py
+++ Task2_(Bank)/Bank/handler.py
@@ -20,26 +20,3 @@ def handler(input_text: str) -> str:
         else:
             break
     return '\n'.join(output_text)
-
-# def handler(script: str) -> str:
-#     def _unknown_command(*_, **__):
-#         print(_, __)
-#         return 'Unknown command'
-#
-#     result = list()
-#     n = 0
-#     print(script.split('\n'))
-#     for line in script.split('\n'):
-#         if n < settings.COMMANDS_MAX_COUNT:
-#             if line != '' and not line.isspace():
-#                 words = line.split()
-#                 command_name, arguments = words[0], words[1:]
-#                 print(command_name)
-#                 result.append(getattr(commands, command_name.lower(), _unknown_command)(*arguments))
-#         else:
-#             break
-#         n += 1
-#     if len(result):
-#         return '\n'.join(result)
-#     else:
-#         return 'Enter the commands!!!'
