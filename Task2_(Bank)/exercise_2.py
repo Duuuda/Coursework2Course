0217e4c2b39c93e59d86e9b21ef26b5b6da0b907
Duuuda <supermen.tut.103103@gmail.com>
2021-05-12 23:23:22 +0300

    Update exercise_2.py

--- Task2_(Bank)/exercise_2.py
+++ Task2_(Bank)/exercise_2.py
@@ -5,26 +5,27 @@ from threading import Thread
 from time import sleep
 
 
+def _open_site(site):
+    def _is_django_available(_site):
+        try:
+            request = get(_site, timeout=1)
+        except ConnectionError:
+            return False
+        if request.status_code != 200:
+            return False
+        return True
+
+    while not _is_django_available(site):
+        sleep(1)
+
+    system(f'start {site}')
+
+
+def main():
+    thread = Thread(target=_open_site, args=('http://127.0.0.1:9000/',), daemon=True)
+    thread.start()
+    system('python manage.py lazyrunserver --port 9000 --no_collect_static --no_re_migrate')
+
+
 if __name__ == '__main__':
-    def _open_site(site):
-        def _is_django_available(_site):
-            try:
-                request = get(_site, timeout=1)
-            except ConnectionError:
-                return False
-            if request.status_code != 200:
-                return False
-            return True
-
-        while not _is_django_available(site):
-            sleep(1)
-
-        system(f'start {site}')
-
-    def main():
-        thread = Thread(target=_open_site, args=('http://127.0.0.1:9000/',), daemon=True)
-        thread.start()
-        system('python manage.py lazyrunserver --port 9000 --no_collect_static --no_re_migrate')
-
-
     main()
